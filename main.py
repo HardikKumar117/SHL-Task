587dd6bdf85d8f7ba1d7832d0facba39c3e3b53c
HardikKumar117 <hardikkumar885@gmail.com>
2026-07-02 22:54:16 +0530

    fixing deployment

--- main.py
+++ main.py
@@ -6,12 +6,17 @@ from langchain_core.messages import HumanMessage
 from agent import Agent
 
 
+
 def main(conv:str):
+    print("building docs")
     docs=build_docs()
+    print("building vector store")
     collection=build_vector_store(docs)
    
     info= extract_hiring_requirements(conv)
+    print("built requirements")
     query=build_query(info)
+    print("invoking agent ")
     agent_response = Agent.invoke(
         {
             "messages": [
@@ -33,20 +38,20 @@ def main(conv:str):
     return agent_response
 
 
-if __name__ == "__main__":
-    main("""{
-  "messages": [
-    {
-      "role": "user",
-      "content": "We need a solution for senior leadership."
-    },
-    {
-      "role": "assistant",
-      "content": "Who is this intended for?"
-    },
-    {
-      "role": "user",
-      "content": "CXOs and directors with 15+ years experience."
-    }
-  ]
-}""")
+# if __name__ == "__main__":
+#     main("""{
+#   "messages": [
+#     {
+#       "role": "user",
+#       "content": "We need a solution for senior leadership."
+#     },
+#     {
+#       "role": "assistant",
+#       "content": "Who is this intended for?"
+#     },
+#     {
+#       "role": "user",
+#       "content": "CXOs and directors with 15+ years experience."
+#     }
+#   ]
+# }""")
